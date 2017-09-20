b700ba93af6901071dc39429dd8e3e4f54a4661f
prudywsh <antoineprudhomme5@gmail.com>
2017-09-20 14:15:07 +0200

    on beer creation, can upload image

--- src/components/dashboard/event/AddBeer.jsx
+++ src/components/dashboard/event/AddBeer.jsx
@@ -13,6 +13,7 @@ export default class AddBeer extends React.Component {
 
         this._submitCreateForm = this._submitCreateForm.bind(this);
         this._handleChange = this._handleChange.bind(this);
+        this._handleFileUpload = this._handleFileUpload.bind(this);
     }
 
     _handleChange(field, e) {
@@ -21,8 +22,17 @@ export default class AddBeer extends React.Component {
         this.setState(nextState);
     }
 
+    _handleFileUpload(e) {
+        this.setState({ file: e.target.files[0] });
+    }
+
     _submitCreateForm() {
-        BeerActions.createBeer(this.state);
+        // Create a new FormData object and fill it with the state values
+        var form = new FormData();
+        Object.keys(this.state).map(attr => {
+            form.append(attr, this.state[attr]);
+        });
+        BeerActions.createBeer(form);
     }
 
     render() {
@@ -45,6 +55,10 @@ export default class AddBeer extends React.Component {
                             <div className="form-group">
                                 <label htmlFor="description">Description</label>
                                 <textarea onChange={e => this._handleChange('description', e)} className="form-control" id="description" rows="3"></textarea>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="img">Image</label>
+                                <input type="file" onChange={this._handleFileUpload} className="form-control-file" id="img" />
                             </div>
                             <div className="text-center">
                                 <div className="btn-group" role="group">
