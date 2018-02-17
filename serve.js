8bcd4e2bd34d2356d0621a18d55080db47329922
prudywsh <antoineprudhomme5@gmail.com>
2018-02-17 16:22:39 +0100

    'cannot get route' problem fixed

--- serve.js
+++ serve.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('build'));
+app.get('*', (req, res) => {
+  res.sendfile('./build/index.html');
+});
 
 // run server
 app.listen('8080', _ => console.log('Server is listening on port 8080...'))
